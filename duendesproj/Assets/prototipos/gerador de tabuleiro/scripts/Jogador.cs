d7a57024d13a3712ba0580dec2eb8d351d5e046b
nicolas-ft <nicolas.andalaft@hotmail.com>
2019-09-02 17:16:42 -0300

    Prototipo Gerador de Tabuleiro
    Funcionando 25% do necessario

---
+++ duendesproj/Assets/prototipos/gerador de tabuleiro/scripts/Jogador.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public Transform casaAtual;
+
+    public void setCasa(Transform novaCasa)
+    {
+        casaAtual = novaCasa;
+        transform.Translate(casaAtual.position, Space.World);
+    }
+
+    public void MoveProximo(int i)
+    {
+        Transform proximaCasa = casaAtual.GetComponent<CasaBase>().proximaCasa[i];
+        casaAtual = proximaCasa;
+        transform.position = casaAtual.position;
+    }
+}
