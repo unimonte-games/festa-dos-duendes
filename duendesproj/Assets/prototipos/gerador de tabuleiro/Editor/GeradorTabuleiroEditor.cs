3a10b9064849dd4cea6f746984d17a9e644c7e8d
Nicolas <nicolas.andalaft@hotmail.com>
2019-09-17 12:13:09 -0300

    Script CustomEditor
    Botão de gerar tabuleiro no modo de edição

---
+++ duendesproj/Assets/prototipos/gerador de tabuleiro/Editor/GeradorTabuleiroEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GeradorTabuleiro))]
+public class GeradorTabuleiroEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GeradorTabuleiro gt = (GeradorTabuleiro)target;
+
+        if (GUILayout.Button("Gerar Tabuleiro"))
+            gt.GerarCasas();
+    }
+}
