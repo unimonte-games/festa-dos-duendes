01fdaa04ee85f93ee1d1748dbfd3b6b91fc4288c
Nicolas <nicolas.andalaft@hotmail.com>
2019-10-02 14:09:30 -0300

    Movimentação escolhendo a rota

---
+++ duendesproj/Assets/prototipos/gerador de tabuleiro/scripts/EscolheRota.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EscolheRota : MonoBehaviour
+{
+    public Movimentacao jogador;
+    public GameObject canvasCarta, canvasDirecao;
+    private int indice = 0;
+
+    public void EscolherRota(bool confirmacao)
+    {
+        CasaBase _casaBase = jogador.casaAtual.GetComponent<CasaBase>();
+        Transform casaTemp = _casaBase.casaSeguinte[indice];
+
+        if (confirmacao)
+        {
+            jogador.setCasa(casaTemp); //Avança na rota escolhida
+            jogador.ProcuraCasa(jogador.proximaCor); //Avança para a cor certa
+
+            canvasCarta.SetActive(true);
+            canvasDirecao.SetActive(false);
+        }
+        else
+        {
+            indice = ++indice % _casaBase.casaSeguinte.Count;
+            Debug.Log("Casa " + indice);
+            casaTemp = _casaBase.casaSeguinte[indice];
+        }
+    }
+}
