054cd6201a8447b41521e69851d71d48598d1eaa
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-10-02 15:38:11 -0300

    continuando balde das maçãs

--- duendesproj/Assets/scripts/Componentes/Jogador/ControladorBaldeDasMacas.cs
+++ duendesproj/Assets/scripts/Componentes/Jogador/ControladorBaldeDasMacas.cs
@@ -2,17 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControladorBaldeDasMacas : MonoBehaviour
+namespace Componentes.Jogador
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ControladorBaldeDasMacas : MonoBehaviour
     {
-        
-    }
+        int macasPegas;
+        bool emPulo;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Transform tr;
+        Controlador ctrl;
+        Movimentador mov;
+        Gerenciadores.GerenciadorBaldeDasMacas gerenBM;
+
+        static readonly Vector3 v3_r = Vector3.right;
+
+        void Awake ()
+        {
+            tr = GetComponent<Transform>();
+            ctrl = GetComponent<Controlador>();
+            mov = GetComponent<Movimentador>();
+            gerenBM = FindObjectOfType<Gerenciadores.GerenciadorBaldeDasMacas>();
+        }
+
+        void Start ()
+        {
+            mov.velocidade = gerenBM.velocidadeMov;
+        }
+
+        void Update()
+        {
+            Controlador.EntradaJogador entradaJogador =
+                ctrl.ObterEntradaJogador();
+
+            if (!emPulo)
+            {
+                float posx = Mathf.Abs(tr.position.x);
+
+                if (posx >= gerenBM.limX)
+                    mov.direcao = Vector3.zero;
+                else
+                {
+                    mov.direcao = v3_r * entradaJogador.eixoH;
+
+                    if (entradaJogador.acao1)
+                    {
+                        emPulo = true;
+                        Pular();
+                    }
+                }
+            }
+
+        }
+
+        public void PontuarMaca()
+        {
+            macasPegas++;
+        }
+
+        void Pular()
+        {
+            StartCoroutine(Co_Pulo());
+        }
+
+        IEnumerator Co_Pulo()
+        {
+            float tPulo = 0;
+            float altura = 0;
+
+            Vector3 pos = tr.position;
+
+            while(emPulo)
+            {
+                tPulo += Time.deltaTime * gerenBM.velPulo;
+                altura = Mathf.Sin(tPulo) * gerenBM.tamPulo;
+
+
+                if (altura <= 0)
+                {
+                    emPulo = false;
+                    altura = 0;
+                }
+
+                pos = tr.position;
+                pos.y = altura;
+                tr.position = pos;
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            pos.y = 0;
+            tr.position = pos;
+        }
     }
 }
