df79d4fa600ec5d45ebdf8c3efd42d36d352b4d7
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-11-01 01:59:16 -0300

    revertendo decisão anterior

--- duendesproj/Assets/scripts/Componentes/Jogador/SpriteLookAtCam.cs
+++ duendesproj/Assets/scripts/Componentes/Jogador/SpriteLookAtCam.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SpriteLookAtCam : MonoBehaviour
 {
-    public bool usarWorldUp;
     Transform cam, tr;
 
     void Awake()
@@ -16,9 +15,6 @@ public class SpriteLookAtCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (usarWorldUp)
-            tr.LookAt(cam);
-        else
-            tr.LookAt(cam, (Vector3.up + Vector3.forward).normalized);
+        tr.LookAt(cam);
     }
 }
