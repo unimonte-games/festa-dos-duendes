2f5890b4a4dd81c43bf6a4b76839d7b19653be56
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-11-01 01:56:38 -0300

    sprite agora olha pro jogador

---
+++ duendesproj/Assets/scripts/Componentes/Jogador/SpriteLookAtCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLookAtCam : MonoBehaviour
+{
+    Transform cam, tr;
+
+    void Awake()
+    {
+        cam = FindObjectOfType<Camera>().GetComponent<Transform>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.LookAt(cam, (Vector3.up + Vector3.forward).normalized);
+    }
+}
