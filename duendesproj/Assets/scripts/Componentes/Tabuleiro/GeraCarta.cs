183f6398f8b0b808c09134300d7371330bec839f
Nicolas <nicolas.andalaft@hotmail.com>
2019-11-08 17:59:36 -0300

    Geração de cartas com probabilidade

--- duendesproj/Assets/scripts/Componentes/Tabuleiro/GeraCarta.cs
+++ duendesproj/Assets/scripts/Componentes/Tabuleiro/GeraCarta.cs
@@ -1,27 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Identificadores;
 
 namespace Componentes.Tabuleiro
 {
     public class GeraCarta : MonoBehaviour
     {
         public Image botao;
-        //public Gerenciadores.GerenciadorPartida _gerenPartida;
-        //public GeradorTabuleiro _geraTabuleiro;
-        private int qtdCasas;
-
-        private void Awake()
-        {
-            //qtdCasas = _geraTabuleiro.coresCasas.Count;
-        }
+        public Gerenciadores.GerenciadorPartida _gerenPartida;
+        public GeradorTabuleiro _geraTabuleiro;
 
         public void GerarCarta()
         {
-            int rand = Random.Range(0, qtdCasas);
+            float rand = Random.value;
+            TiposCasa carta;
 
-            //botao.color = _geraTabuleiro.coresCasas[rand].color;
+            if (rand <= 0.1f)
+                carta = TiposCasa.BemMal;
+            else if (rand <= 0.2f)
+                carta = TiposCasa.Garrafa;
+            else if (rand <= 0.35f)
+                carta = TiposCasa.Acontecimento;
+            else if (rand <= 0.55f)
+                carta = TiposCasa.PowerUp;
+            else if (rand <= 0.9f)
+                carta = TiposCasa.Moeda;
+            else
+                carta = TiposCasa.MiniJogo;
 
-            //_gerenPartida.MoverJogador(rand + 1);
+            Debug.Log(carta);
+
+            _gerenPartida.MoverJogador((int)carta);
         }
     }
 }
