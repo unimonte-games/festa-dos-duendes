12427f205e6a5bc8f4fad75166ec0cac2344d00d
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-11-14 00:01:28 -0300

    Animações

--- duendesproj/Assets/scripts/Componentes/Jogador/Movimentacao.cs
+++ duendesproj/Assets/scripts/Componentes/Jogador/Movimentacao.cs
@@ -11,6 +11,7 @@ namespace Componentes.Jogador
         public Gerenciadores.GerenciadorPartida _gerenPart;
         public Transform casaAtual;
         public TiposCasa proximaCor;
+        public bool emPulinho;
 
         //Animar pulo
         private float duracaoPulo = 0.25f;
@@ -71,6 +72,8 @@ namespace Componentes.Jogador
 
         public IEnumerator Pulinho(Vector3 destino, float tempoInicio)
         {
+            emPulinho = true;
+
             Vector3 centro = (transform.position + destino) * 0.5F;
             centro -= Vector3.up;
 
@@ -86,6 +89,8 @@ namespace Componentes.Jogador
 
             if (x <= 1)
                 yield return StartCoroutine(Pulinho(destino, tempoInicio));
+            else
+                emPulinho = false;
         }
     }
 }
