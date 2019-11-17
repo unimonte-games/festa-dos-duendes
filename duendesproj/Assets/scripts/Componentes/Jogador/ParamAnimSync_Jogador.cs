9b63a7f3ce731a3359e1ff61c74903a665d57765
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-11-17 17:20:17 -0300

    Fazendo o personagem mudar de lado de acordo com a direção onde tá andando

--- duendesproj/Assets/scripts/Componentes/Jogador/ParamAnimSync_Jogador.cs
+++ duendesproj/Assets/scripts/Componentes/Jogador/ParamAnimSync_Jogador.cs
@@ -11,6 +11,7 @@ namespace Componentes.Jogador
         Movimentador movimentador;
         Movimentacao movimentacao;
         Controlador ctrl;
+        Transform spriteTr, tr;
 
         void Awake()
         {
@@ -18,6 +19,8 @@ namespace Componentes.Jogador
             movimentador = GetComponent<Movimentador>();
             movimentacao = GetComponent<Movimentacao>();
             ctrl = GetComponent<Controlador>();
+            tr = GetComponent<Transform>();
+            spriteTr = tr.GetChild(0);
         }
 
         void Start()
@@ -25,6 +28,8 @@ namespace Componentes.Jogador
             animator.SetInteger(
                 "cena atual", SceneManager.GetActiveScene().buildIndex
             );
+
+            StartCoroutine(AjustadorDeLado());
         }
 
         void Update()
@@ -51,6 +56,39 @@ namespace Componentes.Jogador
             }
 
             return false;
+        }
+
+        IEnumerator AjustadorDeLado()
+        {
+            // 1: antes; 2: agora
+            float x1 = 0f, x2 = 0f, z1 = 0f, z2 = 0f;
+            Vector3 escalaOriginal = tr.localScale;
+            Vector3 escala = escalaOriginal;
+
+            while (true)
+            {
+                x1 = x2;
+                z1 = z2;
+
+                yield return new WaitForSeconds(0.1f);
+
+                x2 = tr.position.x;
+                z2 = tr.position.z;
+
+                float xDiff = x2 - x1;
+                float zDiff = z2 - z1;
+                float lado = 0f;
+
+                if (xDiff < -0.01f || zDiff < -0.01f)
+                    lado = -1f;
+                else if (xDiff > 0.01f || zDiff > 0.01f)
+                    lado = 1f;
+
+                float ladoAbs = Mathf.Abs(lado);
+
+                escala.x = escalaOriginal.x * (ladoAbs > 0.2f ? lado : 1f);
+                tr.localScale = escala;
+            }
         }
     }
 }
