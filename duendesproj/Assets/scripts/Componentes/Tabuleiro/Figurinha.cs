f5019fbe7dfe60d484c12b766dca4d0ee44e2b68
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-11-18 12:33:57 -0300

    Código instanciador de figurinhas

---
+++ duendesproj/Assets/scripts/Componentes/Tabuleiro/Figurinha.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Identificadores;
+
+namespace Componentes.Tabuleiro
+{
+    public class Figurinha : MonoBehaviour
+    {
+        public Sprite figMinijogo;
+        public Sprite figMoeda;
+        public Sprite figBemMal;
+        public Sprite figPowerUp;
+        public Sprite figGarrafa;
+        public Sprite figAcontecimentoAleatorio;
+
+        void Start()
+        {
+            Transform casas = transform.Find("Casas");
+            Transform figurinhas = transform.Find("Figurinhas");
+
+            for (int i = 0; i < casas.childCount; i++)
+            {
+                Transform casaBase_tr = casas.GetChild(i);
+                CasaBase casaBase = casaBase_tr.GetComponent<CasaBase>();
+
+                if (casaBase.tipoCasa == TiposCasa.Moeda)
+                    Instanciar(figMoeda, casaBase_tr, figurinhas);
+                else if (casaBase.tipoCasa == TiposCasa.BemMal)
+                    Instanciar(figBemMal, casaBase_tr, figurinhas);
+                else if (casaBase.tipoCasa == TiposCasa.PowerUp)
+                    Instanciar(figPowerUp, casaBase_tr, figurinhas);
+                else if (casaBase.tipoCasa == TiposCasa.Garrafa)
+                    Instanciar(figGarrafa, casaBase_tr, figurinhas);
+                else if (casaBase.tipoCasa == TiposCasa.Acontecimento)
+                    Instanciar(figAcontecimentoAleatorio, casaBase_tr, figurinhas);
+            }
+        }
+
+        void Instanciar(Sprite spr, Transform casaBase_tr, Transform figurinhas)
+        {
+            GameObject novaFigurinha = Instantiate<GameObject>(
+                new GameObject("figurinha"),
+                casaBase_tr.position + (casaBase_tr.up * 0.05f),
+                Quaternion.Euler(90f, 0f, 0f),
+                figurinhas
+            );
+
+            var sprRend = novaFigurinha.AddComponent<SpriteRenderer>();
+            sprRend.sprite = spr;
+        }
+    }
+}
