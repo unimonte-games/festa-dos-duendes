bdb361b6476795af227f799c66e66d17bd2686f4
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-11-19 17:06:40 -0300

    modificações no tabuleiro
    adição de componente figurinha e definição dos prefabs dos jogadores.

--- duendesproj/Assets/scripts/Componentes/Tabuleiro/Figurinha.cs
+++ duendesproj/Assets/scripts/Componentes/Tabuleiro/Figurinha.cs
@@ -34,17 +34,20 @@ namespace Componentes.Tabuleiro
                     Instanciar(figGarrafa, casaBase_tr, figurinhas);
                 else if (casaBase.tipoCasa == TiposCasa.Acontecimento)
                     Instanciar(figAcontecimentoAleatorio, casaBase_tr, figurinhas);
+                else if (casaBase.tipoCasa == TiposCasa.MiniJogo)
+                    Instanciar(figMinijogo, casaBase_tr, figurinhas);
             }
         }
 
         void Instanciar(Sprite spr, Transform casaBase_tr, Transform figurinhas)
         {
-            GameObject novaFigurinha = Instantiate<GameObject>(
-                new GameObject("figurinha"),
-                casaBase_tr.position + (casaBase_tr.up * 0.05f),
-                Quaternion.Euler(90f, 0f, 0f),
-                figurinhas
-            );
+            GameObject novaFigurinha = new GameObject("figurinha");
+            Transform novaFigurinha_tr = novaFigurinha.transform;
+
+            novaFigurinha_tr.position =
+                casaBase_tr.position + (casaBase_tr.up * 0.05f);
+            novaFigurinha_tr.rotation = Quaternion.Euler(90f, 0f, 0f);
+            novaFigurinha_tr.SetParent(figurinhas);
 
             var sprRend = novaFigurinha.AddComponent<SpriteRenderer>();
             sprRend.sprite = spr;
