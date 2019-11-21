78c1055549f0bb751d023319787cc10435bc90c2
Nicolas <nicolas.andalaft@hotmail.com>
2019-11-21 05:21:54 -0300

    Começo da HUD

---
+++ duendesproj/Assets/scripts/Telas/TabuleiroHUD.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Gerenciadores;
+
+namespace Telas
+{
+    public class TabuleiroHUD : MonoBehaviour
+    {
+        public RectTransform painelPrincipal;
+        public GameObject painelPrefab;
+        [HideInInspector]
+        public Image[] fundos = new Image[GerenciadorGeral.qtdJogadores];
+
+        private void Start()
+        {
+            if (fundos[0] == null)
+            {
+                for (int i = 0; i < GerenciadorGeral.qtdJogadores; i++)
+                {
+                    GameObject painel = Instantiate(painelPrefab);
+                    painel.transform.SetParent(painelPrincipal);
+
+                    fundos[i] = painel.GetComponentInChildren<Image>();
+                    fundos[i].color = Color.gray;
+                }
+            }
+        }
+    }
+}
